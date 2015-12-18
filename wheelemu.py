3350b0986559e6e75d79b4d39861424286c0549b
Grady O'Connell <flipcoder@gmail.com>
2015-12-18 07:27:19 -0800

    script

---
+++ wheelemu.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python2
+
+from pymouse import PyMouse
+from pymouse import PyMouseEvent
+from pykeyboard import PyKeyboard
+from pykeyboard import PyKeyboardEvent
+import time
+
+m = PyMouse()
+k = PyKeyboard()
+
+scrolling = False
+
+mx = 0
+my = 0
+threshold = 8
+
+class MoveEvent(PyMouseEvent):
+    def __init__(self):
+        PyMouseEvent.__init__(self)
+    def move(self, x, y):
+        global scrolling
+        global m, mx, my
+        if scrolling:
+            # simulate scroll
+            while True:
+                if x < mx - threshold:
+                    m.click(x, y, 6)
+                    mx = x - threshold
+                elif x > mx + threshold:
+                    m.click(x, y, 7)
+                    mx = x + threshold
+                elif y < my - threshold:
+                    m.click(x, y, 4)
+                    my = y - threshold
+                elif y > my + threshold:
+                    m.click(x, y, 5)
+                    my = y + threshold
+                else:
+                    break
+        else:
+            mx, my = x, y
+
+class HoldEvent(PyKeyboardEvent):
+    def __init__(self):
+        PyKeyboardEvent.__init__(self)
+    def tap(self, key, ch, press):
+        global scrolling
+        if key == 173:
+            if scrolling != press:
+                scrolling = press
+
+hold_ev = HoldEvent()
+hold_ev.start()
+move_ev = MoveEvent()
+move_ev.start()
+
+try:
+    while True:
+        time.sleep(1)
+except:
+    move_ev.stop()
+    hold_ev.stop()
+
